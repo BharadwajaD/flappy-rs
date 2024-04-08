9a77f70d596116f963b4e5eb9e2fd89d4dd6d856
BharadwajaD <trail.tech.blazer@gmail.com>
2024-04-08 21:55:30 +0530

    debug: correct tcp parsing

--- init.lua
+++ init.lua
@@ -1,9 +1,12 @@
 local uv = vim.loop
+local api = vim.api
 
-local window = require("window")
+local window = require("window.window")
 local render = require("game.render")
 local tcp = require("tcp")
+local queue = require("queue")
 
+-- TODO: Don't know wat to do with this
 local group = vim.api.nvim_create_augroup("flappy.window", {
     clear = true
 })
@@ -14,41 +17,49 @@ function START()
 
     local client = uv.new_tcp()
     M.client = client
+    local pqueue = queue:new(4)
+    local buffer
+
 
     client:connect("127.0.0.1", 42069, function (err)
         print("client created & connected to server")
     end)
+    client:read_start(
+        vim.schedule_wrap(
+            function (err, chunk)
 
-    client:read_start(vim.schedule_wrap(function (err, chunk)
+                print("DEBUG:CHUNK: ", chunk)
+                for _, cmd in pairs(tcp.process_packets(chunk)) do
+                    local obj = cmd[1]
+                    local param1 = tonumber(cmd[2])
+                    local param2 = tonumber(cmd[3])
 
-        client:write(chunk)
+                    if obj == "S" and M.win_config ~= nil then
+                        M.win_config = window.create_window({width = param1, height = param2})
+                        buffer = M.win_config.buffer
+                    end
 
-        local obj = tcp.parse(chunk)
-        assert(obj, "string parsing went wrong")
+                    if M.win_config == nil then
+                        M.win_config = window.create_window({width = 80, height = 24})
+                        buffer = M.win_config.buffer
+                    end
 
-        local param1 = tonumber(obj[2])
-        local param2 = tonumber(obj[3])
+                    if obj == "P" then
+                        local rpipe = pqueue:enqueue({param1 = param1, param2 = param2})
+                        if rpipe ~= nil then
+                            render.remove_pipe(M.win_config.buffer, rpipe.param1, rpipe.param2)
+                        end
+                        render.add_pipe(M.win_config.buffer,  param1, param2)
+                    else
+                        render.update_bird(M.win_config.buffer, param1, param2)
+                    end
 
-        if obj[1] == "S" then
-            if  M.win_config ~= nil then
-                M.win_config = window.create_window({width = param1, height = param2})
-            end
-        elseif obj[1] == "P" then
-            if M.win_config == nil then
-                M.win_config = window.create_window({width = 80, height = 24})
-            end
-            render.draw_pipe(M.win_config.dim, M.win_config.buffer_id,  param1, param2)
-        else
-            if M.win_config == nil then
-                M.win_config = window.create_window({width = 80, height = 24})
-            end
-            render.draw_bird(M.win_config.dim, M.win_config.buffer_id, param1, param2)
-        end
-    end))
+                    buffer:render()
+                end
+            end))
 end
 
 function END()
     assert(M.client, "client already closed")
     M.client:read_stop()
 end
-
--- lua/tcp.lua
+++ lua/tcp.lua
@@ -1,25 +1,34 @@
 local M = {}
 
--- Commands are seperated by ?
+-- Define buffer as a global variable
+local buffer = ""
+
+-- Commands are separated by ?
 -- Example: P:12:24?B:11:12?...
-local stream = ""
-local cmd_sep = "?"
-local sep = ":"
-
-function M.parse(chunk)
-
-    stream = stream .. chunk
-    local idx = string.find(stream, cmd_sep)
-
-    assert(idx, "no ? in stream")
-
-    local cmd = string.sub(stream, 1, idx-1)
-    stream = string.sub(stream, idx+1, string.len(stream))
-
+function M.process_packets(chunk)
+    local stream = buffer .. chunk
+    local cmd_sep = "?"
+    local sep = ":"
     local splits = {}
-    for split in string.gmatch(cmd, "([^"..sep.."]+)") do
-        table.insert(splits, split)
+
+    local idx = string.find(stream, cmd_sep)
+    while idx do
+        local cmd = string.sub(stream, 1, idx - 1)
+        stream = string.sub(stream, idx + 1)
+
+        local cmd_splits = {}
+        for split in string.gmatch(cmd, "([^" .. sep .. "]+)") do
+            table.insert(cmd_splits, split)
+        end
+
+        table.insert(splits, cmd_splits)
+
+        idx = string.find(stream, cmd_sep)
     end
+
+    -- Update the global buffer
+    buffer = stream
+
     return splits
 end
 
